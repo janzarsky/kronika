222d4b0c6b86b974de57f0ddc99d90905e230a88
Jan Žárský <jan.zarsky@gmail.com>
2014-08-03 10:41:09 +0200

    Add css classes

--- application/views/events/index.php
+++ application/views/events/index.php
@@ -1,18 +1,26 @@
 <?php foreach ($events as $event) : ?>
 	<?php if (isset($event['year_header'])): ?>
-		<header>
+		<header class="dateHeader--year">
 			<?php echo $event['year_header']; ?>
 		</header>
 	<?php endif; ?>
 	<?php if (isset($event['month_header'])): ?>
-		<header>
+		<header class="dateHeader--month">
 			<?php echo $event['month_header']; ?>
 		</header>
 	<?php endif; ?>
 	
-	<section>
-		<header><?php echo $event['title']; ?></header>
-		<?php echo $event['text']; ?>
-		<footer><?php echo $event['date']; ?></footer>
+	<section class="event">
+		<header class="event__title">
+			<?php echo $event['title']; ?>
+		</header>
+		
+		<p class="event__text">
+			<?php echo $event['text']; ?>
+		</p>
+		
+		<footer class="event__date">
+			<?php echo $event['date']; ?>
+		</footer>
 	</section>
 <?php endforeach; ?>
