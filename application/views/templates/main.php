00c95d3a32e6476a632e169a5efd462ada0a8012
Jan Žárský <jan.zarsky@gmail.com>
2014-08-03 12:03:25 +0200

    Add Bootstrap to project

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -4,6 +4,8 @@
 	<title>Kronika</title>
 	<meta charset="UTF-8">
 	<?php	echo meta(array('name' => 'description', 'content' => 'Website for keeping important events from history')); ?>
+	
+	<link href="public/stylesheets/bootstrap-custom.css" rel="stylesheet">
 </head>
 <body>
 	<?php
@@ -19,5 +21,8 @@
 			$this->load->view('templates/nav');
 	?>
 	<?php echo $content; ?>
+	
+	<script src="public/javascripts/jquery.min.js"></script>
+	<script src="public/javascripts/bootstrap.min.js"></script>
 </body>
 </html>
