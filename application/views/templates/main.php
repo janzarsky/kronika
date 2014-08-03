06e493d9e5683b4cb26c92b0f925e10d33dabd12
Jan Žárský <jan.zarsky@gmail.com>
2014-08-03 12:09:11 +0200

    Main template change to fit bootstrap

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -1,26 +1,29 @@
 <?php echo doctype('html5'); ?>
-<html>
+<html lang="en">
 <head>
 	<title>Kronika</title>
 	<meta charset="UTF-8">
-	<?php	echo meta(array('name' => 'description', 'content' => 'Website for keeping important events from history')); ?>
+	<meta name="description" content="Website for keeping important events from history" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	
 	<link href="public/stylesheets/bootstrap-custom.css" rel="stylesheet">
 </head>
 <body>
-	<?php
-		if (isset($header))
-			echo $header;
-		else
-			$this->load->view('templates/header');
-	?>
-	<?php
-		if (isset($nav))
-			echo $nav;
-		else
-			$this->load->view('templates/nav');
-	?>
-	<?php echo $content; ?>
+	<div class="container">
+		<?php
+			if (isset($header))
+				echo $header;
+			else
+				$this->load->view('templates/header');
+		?>
+		<?php
+			if (isset($nav))
+				echo $nav;
+			else
+				$this->load->view('templates/nav');
+		?>
+		<?php echo $content; ?>
+	</div>
 	
 	<script src="public/javascripts/jquery.min.js"></script>
 	<script src="public/javascripts/bootstrap.min.js"></script>
