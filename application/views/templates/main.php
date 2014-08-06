90bf151190fc37ac004455cc5a3a91f57e444834
Jan Žárský <jan.zarsky@gmail.com>
2014-08-06 17:18:00 +0200

    Import css file in main template

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -7,6 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 	
 	<link href="<?php echo stylesheet('bootstrap-custom.css'); ?>" rel="stylesheet">
+	<link href="<?php echo stylesheet('screen.css'); ?>" rel="stylesheet">
 </head>
 <body>
 	<div class="container">
