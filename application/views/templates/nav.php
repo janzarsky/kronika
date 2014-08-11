225da22dc5b880879b3f73ea597896fa45c0e9c9
Jan Žárský <jan.zarsky@gmail.com>
2014-08-11 11:42:20 +0200

    Create mobile first navigation

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -10,27 +10,30 @@
 	?>
 	
 	<?php for($decade = $last_decade; $decade >= $first_decade; $decade -= 10): ?>
-		<div class="navbar__panel">
-			<a href="<?php echo $decade + 9; ?>">
-				<header class="navbar__panelHeading">
-					<?php echo substr($decade, 2, 2) . '\''; ?>
-				</header>
-			</a>
-			
-			<?php $collapsed = (($active_year - $decade) < 10 && ($active_year - $decade) >= 0) ? "" : "navbar__panelCollapse--collapsed"; ?>
-			
-			<div class="navbar__panelCollapse <?php echo $collapsed; ?>">
-				<ul class="navbar__itemGroup">
-					<?php for($year = $decade + 9; $year >= $decade; $year--): ?>
+		<?php $decade_year = (($decade + 9) > $last_year) ? $last_year : $decade + 9; ?>
+		
+		<?php $active = (($active_year - $decade) < 10 && ($active_year - $decade) >= 0) ? "navbar__panelHeading--active" : ""; ?>
+		<a href="<?php echo $decade_year; ?>">
+			<header class="navbar__panelHeading <?php echo $active; ?>">
+				<?php echo substr($decade, 2, 2) . '\''; ?>
+			</header>
+		</a>
+		
+		<?php $collapsed = (($active_year - $decade) < 10 && ($active_year - $decade) >= 0) ? "" : "navbar__panelCollapse--collapsed"; ?>
+		
+		<div class="navbar__panelCollapse <?php echo $collapsed; ?>">
+			<ul class="navbar__itemGroup">
+				<?php for($year = $decade + 9; $year >= $decade; $year--): ?>
+					<?php if ($year >= $first_year && $year <= $last_year): ?>
 						<?php $active = ($year == $active_year) ? "navbar__item--active" : ""; ?>
 						<a href="<?php echo $year; ?>">
 							<li class="navbar__item <?php echo $active; ?>">
 								<?php echo $year; ?>
 							</li>
 						</a>
-					<?php endfor; ?>
-				</ul>
-			</div>
+					<?php endif; ?>
+				<?php endfor; ?>
+			</ul>
 		</div>
 	<?php endfor; ?>
 </nav>
