72702ff0fbc7ca419936ef9a90899d382a17a6a9
Jan Žárský <jan.zarsky@gmail.com>
2014-10-08 22:11:05 +0200

    Allow zooming

--- application/views/templates/main.php
+++ application/views/templates/main.php
@@ -4,7 +4,7 @@
 	<title>Kronika</title>
 	<meta charset="UTF-8">
 	<meta name="description" content="Website for keeping important events from history" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
 	
 	<link href="<?php echo stylesheet('bootstrap-custom.css'); ?>" rel="stylesheet">
 	<link href="<?php echo stylesheet('screen.css'); ?>" rel="stylesheet">
