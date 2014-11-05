f0f81c4d2285a939ae4f08c32433912a50f0e997
Jan Žárský <jan.zarsky@gmail.com>
2014-11-05 18:42:44 +0100

    Add encryption key

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '7af1daa8eee7997b7ff4ede5b399b4b3';
 
 /*
 |--------------------------------------------------------------------------
