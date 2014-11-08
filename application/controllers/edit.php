6a48178f32e2da334b3b6c1e84398fc29e76d0bc
Jan Žárský <jan.zarsky@gmail.com>
2014-11-08 11:48:44 +0100

    Simple edit page

--- application/controllers/edit.php
+++ application/controllers/edit.php
@@ -6,10 +6,17 @@ class Edit extends CI_Controller {
 	{
 		parent::__construct();
 		$this->load->model('edit_model');
+		$this->load->model('user_model');
 	}
 
 	public function index()
 	{
+		$data['content'] = '';
 		
+		$header_data['name'] = $this->user_model->get_name();
+		$header_data['permissions'] = $this->user_model->get_permissions();
+		$data['header'] = $this->load->view('templates/admin_header', $header_data, true);
+		
+		$this->load->view('templates/main', $data);
 	}
 }
