5a850acc1e325eb344e23373fb167e2b17175713
Jan Žárský <jan.zarsky@gmail.com>
2014-11-09 17:30:07 +0100

    Style edit page

--- application/views/edit/edit.php
+++ application/views/edit/edit.php
@@ -8,55 +8,70 @@
 	</div>
 </div>
 
-<form class="form" role="form" action="" method="post">
-	<div class="row">
-		<div class="col-sm-7 col-sm-push-5">
-			<div class="form-group">
-				<label class="sr-only" for="event_title">Titulek</label>
-				<input type="text" class="form-control" name="title" id="event_title" placeholder="Titulek"
-					value="<?php echo set_value('title', $event['title']); ?>">
-			</div>
-		</div>
-		
-		<div class="col-sm-5 col-sm-pull-7">
-			<div class="form-group">
-				<label class="sr-only" for="event_date">Datum</label>
-				<input type="text" class="form-control" name="date" id="event_date" placeholder="Datum"
-					value="<?php echo set_value('date', $event['date']); ?>">
-			</div>
-		</div>
-	</div>
-	
-	<div class="row">
-		<div class="col-sm-5">
-			<div class="form-group">
-				<label class="sr-only" for="event_url">URL</label>
-				<input type="text" class="form-control" name="url" id="event_url" placeholder="URL"
-					value="<?php echo set_value('url', $event['url']); ?>">
-			</div>
-		</div>
-		
-		<div class="col-sm-7">
-			<textarea class="form-control" name="text" rows="7"><?php echo set_value('text', $event['text']); ?></textarea>
-		</div>
-	</div>
-	
-	<div class="row">
-		<div class="col-sm-7 col-sm-offset-5">
-			<div class="checkbox">
-				<label>
-					<?php if ($can_publish): ?>
-						<input type="checkbox" name="publish" value="1"
-							<?php echo set_checkbox('publish', '1', $event['published'] == 1); ?>> Publikovat
-					<?php else: ?>
-						<input type="checkbox" name="send_for_approval" value="1"
-							<?php echo set_checkbox('send_for_aproval', '1', $event['sent_for_approval'] == 1); ?>> Odeslat ke schválení
-					<?php endif; ?>
-				</label>
+<section class="edit">
+	<form class="form form-horizontal" role="form" action="" method="post">
+		<div class="row">
+			<div class="col-sm-7 col-sm-push-5">
+				<div class="form-group">
+					<label class="sr-only" for="event_title">Titulek</label>
+					<div class="col-sm-12">
+						<input type="text" class="form-control edit__title" name="title" id="event_title" placeholder="Titulek"
+							value="<?php echo set_value('title', $event['title']); ?>">
+					</div>
+				</div>	
+				
+				<textarea class="form-control edit__text" name="text" rows="7"><?php echo set_value('text', $event['text']); ?></textarea>
 			</div>
 			
-			<button type="submit" class="btn btn-primary">Uložit</button>
-			<a class="btn btn-default" href="<?php echo base_url('/archive'); ?>" role="button">Zavřít</a>
+			<div class="col-sm-5 col-sm-pull-7">
+				<div class="row">
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="event_date">Datum</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control edit__date" name="date" id="event_date" placeholder="Datum"
+								value="<?php echo set_value('date', $event['date']); ?>">
+						</div>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="form-group">
+						<label class="col-sm-2 control-label" for="event_url">URL</label>
+						<div class="col-sm-10">
+							<input type="text" class="form-control edit__url" name="url" id="event_url" placeholder="URL"
+								value="<?php echo set_value('url', $event['url']); ?>">
+						</div>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="col-sm-10 col-sm-offset-2">
+						<div class="form-group">
+							<div class="checkbox">
+								<label>
+									<?php if ($can_publish): ?>
+										<input type="checkbox" name="publish" value="1"
+											<?php echo set_checkbox('publish', '1', $event['published'] == 1); ?>> Publikovat
+									<?php else: ?>
+										<input type="checkbox" name="send_for_approval" value="1"
+											<?php echo set_checkbox('send_for_aproval', '1', $event['sent_for_approval'] == 1); ?>> Odeslat ke schválení
+									<?php endif; ?>
+								</label>
+							</div>
+						</div>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="form-group">
+						<div class="col-sm-10 col-sm-offset-2">
+							<button type="submit" class="btn btn-primary">Uložit</button>
+							<a class="btn btn-default" href="<?php echo base_url('/archive'); ?>" role="button">Zavřít</a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-</form>
+
+	</form>
+</section>
