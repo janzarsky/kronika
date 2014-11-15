a3525e1ccd79e20ad0deeeeb9ca534fd3436ae42
Jan Žárský <jan.zarsky@gmail.com>
2014-11-15 15:30:48 +0100

    Extend utility helper

--- application/helpers/utility_helper.php
+++ application/helpers/utility_helper.php
@@ -8,6 +8,9 @@ function javascript($filename){
    return base_url() . 'public/javascripts/' . $filename;
 }
 
-function media_image($filename){
-   return base_url() . 'public/media/images/h1080px/' . $filename . '.jpg';
+function media_image($filename, $height=1080){
+   if ($height == 'thumb')
+      return base_url() . 'public/media/images/thumb/' . $filename . '.jpg';
+   else
+      return base_url() . 'public/media/images/h' . $height . 'px/' . $filename . '.jpg';
 }
