21c3e92412a61051494dce1aaf561022449aa165
Jan Žárský <jan.zarsky@gmail.com>
2014-11-16 09:36:43 +0100

    Srcset fix

--- application/views/templates/event_media.php
+++ application/views/templates/event_media.php
@@ -4,25 +4,27 @@
 			<div class="row">
 				<div class="col-sm-10 col-sm-offset-1">
 					<div class="event__mediaItem">
-						<img srcset="<?php echo media_image($event['main_image_id'], 1080); ?> 1620w,
-												 <?php echo media_image($event['main_image_id'], 768); ?> 1152w,
-												 <?php echo media_image($event['main_image_id'], 420); ?> 630w,
-												 <?php echo media_image($event['main_image_id'], 210); ?> 315w"
+						<img srcset="<?php echo media_image($m['id'], 1080); ?> 1620w,
+												 <?php echo media_image($m['id'], 768); ?> 1152w,
+												 <?php echo media_image($m['id'], 420); ?> 630w,
+												 <?php echo media_image($m['id'], 210); ?> 315w"
 							sizes="100vw"
 							src="<?php echo media_image($m['id'], 1080); ?>"
-							alt="$m['title']"
-							title="<?php echo $m['title']; ?>" />
+							alt="<?php echo (isset($m['title'])) ? $m['title'] : 'Fotka č. ' . $m['id']; ?>"
+							title="<?php echo (isset($m['title'])) ? $m['title'] : ''; ?>" />
 					</div>
 				</div>
 			</div>
 			
-			<div class="row">
-				<div class="col-sm-7 col-sm-offset-5">
-					<div class="event__mediaTitle">
-						<?php echo $m['title']; ?>
+			<?php if (isset($media['title'])): ?>
+				<div class="row">
+					<div class="col-sm-7 col-sm-offset-5">
+						<div class="event__mediaTitle">
+							<?php echo $m['title']; ?>
+						</div>
 					</div>
 				</div>
-			</div>
+			<?php endif; ?>
 		<?php endif; ?>
 	<?php endforeach; ?>
 </div>
