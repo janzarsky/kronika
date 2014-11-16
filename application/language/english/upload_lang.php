d0fd035b596008b1d9cde783c3a0b05ed057e154
Jan Žárský <jan.zarsky@gmail.com>
2014-11-16 13:37:58 +0100

    Translate upload error messages

---
+++ application/language/english/upload_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['upload_userfile_not_set'] = "Unable to find a post variable called userfile.";
+$lang['upload_file_exceeds_limit'] = "Soubor je příliš velký.";
+$lang['upload_file_exceeds_form_limit'] = "Soubor je příliš velký.";
+$lang['upload_file_partial'] = "Nepodařilo se nahrát celý soubor.";
+$lang['upload_no_temp_directory'] = "The temporary folder is missing.";
+$lang['upload_unable_to_write_file'] = "The file could not be written to disk.";
+$lang['upload_stopped_by_extension'] = "Rozšíření prohlížeče přerušilo nahrávání souboru.";
+$lang['upload_no_file_selected'] = "Nevybrali jste žádný soubor k nahrání.";
+$lang['upload_invalid_filetype'] = "Špatný formát souboru.";
+$lang['upload_invalid_filesize'] = "Soubor je příliš velký.";
+$lang['upload_invalid_dimensions'] = "Obrázek převyšuje maximální povolené rozměry.";
+$lang['upload_destination_error'] = "A problem was encountered while attempting to move the uploaded file to the final destination.";
+$lang['upload_no_filepath'] = "The upload path does not appear to be valid.";
+$lang['upload_no_file_types'] = "You have not specified any allowed file types.";
+$lang['upload_bad_filename'] = "The file name you submitted already exists on the server.";
+$lang['upload_not_writable'] = "The upload destination folder does not appear to be writable.";
+
+
+/* End of file upload_lang.php */
+/* Location: ./system/language/english/upload_lang.php */
