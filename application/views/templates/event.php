0d912c996d14798074a2594bbc7d063fd30f45bf
Jan Žárský <jan.zarsky@gmail.com>
2014-11-30 15:14:39 +0100

    Rewrite for sm screen size

--- application/views/templates/event.php
+++ application/views/templates/event.php
@@ -7,19 +7,19 @@
 	
 	<?php if ($importance == 3 ||
 						($importance == 2 && $has_image == false) ||
-						($importance == 1 && $has_image == false && $has_text == false)): ?>
+						($importance == 1 && $has_image == false)): ?>
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-5 col-sm-offset-7">
 				<?php echo $this->load->view('templates/event_title.php', array('type' => null), true); ?>
 			</div>
 			
-			<div class="col-sm-12">
+			<div class="col-sm-5 col-sm-offset-7">
 				<?php echo $this->load->view('templates/event_date.php', array('type' => 'right'), true); ?>
 			</div>
 		</div>
 	<?php elseif ($importance == 2): ?>
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-7">
 				<?php if (isset($event['main_image_id'])): ?>
 					<?php echo $this->load->view('templates/event_main_image.php', array('image_type' => null), true); ?>
 				<?php else: ?>
@@ -28,7 +28,7 @@
 					</p>
 				<?php endif; ?>
 			</div>
-			<div class="col-sm-12">
+			<div class="col-sm-5">
 				<div class="row">
 					<div class="col-sm-12">
 						<?php echo $this->load->view('templates/event_title.php', array('type' => null), true); ?>
@@ -42,21 +42,16 @@
 		</div>
 	<?php elseif ($importance == 1): ?>
 		<div class="row">
-			<div class="col-sm-12">
+			<div class="col-sm-9">
 				<?php echo $this->load->view('templates/event_main_image.php', array('image_type' => 'large'), true); ?>
 			</div>
-			<div class="col-sm-12">
+			<div class="col-sm-3">
 				<div class="row">
 					<div class="col-sm-12">
 						<?php echo $this->load->view('templates/event_title.php', array('type' => null), true); ?>
 					</div>
 					<div class="col-sm-12">
 						<?php echo $this->load->view('templates/event_date.php', array('type' => null), true); ?>
-					</div>
-					<div class="col-sm-12">
-						<p class="event__text event__text--important">
-							<?php echo $event['text']; ?>
-						</p>
 					</div>
 				</div>
 			</div>
--- application/views/templates/event_main_image.php
+++ application/views/templates/event_main_image.php
@@ -1,5 +1,5 @@
 <?php if ($image_type == 'large'): ?>
-	<div class="event__main-image event__main-image--large">
+	<div class="event__mainImage event__mainImage--large">
 		<a href="<?php echo base_url('detail/' . $event['url']); ?>">
 			<img srcset="<?php echo media_image($event['main_image_id'], 1080); ?> 1620w,
 								 <?php echo media_image($event['main_image_id'], 768); ?> 1152w,
@@ -11,7 +11,7 @@
 		</a>
 	</div>
 <?php else: ?>
-	<div class="event__main-image event__main-image--small">
+	<div class="event__mainImage">
 		<a href="<?php echo base_url('detail/' . $event['url']); ?>">
 			<img srcset="<?php echo media_image($event['main_image_id'], 210); ?> 1x,
 									 <?php echo media_image($event['main_image_id'], 420); ?> 2x"
