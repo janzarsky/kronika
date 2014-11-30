ec0dd1872681af782f8450760d70296ab9dfc382
Jan Žárský <jan.zarsky@gmail.com>
2014-11-30 15:07:02 +0100

    Rewrite for mobile only

--- application/views/templates/event.php
+++ application/views/templates/event.php
@@ -9,20 +9,29 @@
 						($importance == 2 && $has_image == false) ||
 						($importance == 1 && $has_image == false && $has_text == false)): ?>
 		<div class="row">
-			<div class="col-sm-7 col-sm-push-5">
+			<div class="col-sm-12">
 				<?php echo $this->load->view('templates/event_title.php', array('type' => null), true); ?>
 			</div>
 			
-			<div class="col-sm-5 col-sm-pull-7">
-				<?php echo $this->load->view('templates/event_date.php', array('type' => null), true); ?>
+			<div class="col-sm-12">
+				<?php echo $this->load->view('templates/event_date.php', array('type' => 'right'), true); ?>
 			</div>
 		</div>
 	<?php elseif ($importance == 2): ?>
 		<div class="row">
-			<div class="col-sm-5">
+			<div class="col-sm-12">
+				<?php if (isset($event['main_image_id'])): ?>
+					<?php echo $this->load->view('templates/event_main_image.php', array('image_type' => null), true); ?>
+				<?php else: ?>
+					<p class="event__text">
+						<?php echo $event['text']; ?>
+					</p>
+				<?php endif; ?>
+			</div>
+			<div class="col-sm-12">
 				<div class="row">
 					<div class="col-sm-12">
-						<?php echo $this->load->view('templates/event_title.php', array('type' => 'left'), true); ?>
+						<?php echo $this->load->view('templates/event_title.php', array('type' => null), true); ?>
 					</div>
 					<div class="col-sm-12">
 						<?php echo $this->load->view('templates/event_date.php', array('type' => null), true); ?>
@@ -30,25 +39,19 @@
 					
 				</div>
 			</div>
-			<div class="col-sm-7">
-				<?php if (isset($event['main_image_id'])): ?>
-					<?php echo $this->load->view('templates/event_main_image.php', array('image_type' => 'left'), true); ?>
-				<?php else: ?>
-					<p class="event__text">
-						<?php echo $event['text']; ?>
-					</p>
-				<?php endif; ?>
-			</div>
 		</div>
 	<?php elseif ($importance == 1): ?>
 		<div class="row">
-			<div class="col-sm-5">
+			<div class="col-sm-12">
+				<?php echo $this->load->view('templates/event_main_image.php', array('image_type' => 'large'), true); ?>
+			</div>
+			<div class="col-sm-12">
 				<div class="row">
 					<div class="col-sm-12">
-						<?php echo $this->load->view('templates/event_title.php', array('type' => 'important'), true); ?>
+						<?php echo $this->load->view('templates/event_title.php', array('type' => null), true); ?>
 					</div>
 					<div class="col-sm-12">
-						<?php echo $this->load->view('templates/event_date.php', array('type' => 'important'), true); ?>
+						<?php echo $this->load->view('templates/event_date.php', array('type' => null), true); ?>
 					</div>
 					<div class="col-sm-12">
 						<p class="event__text event__text--important">
@@ -56,9 +59,6 @@
 						</p>
 					</div>
 				</div>
-			</div>
-			<div class="col-sm-7">
-				<?php echo $this->load->view('templates/event_main_image.php', array('image_type' => 'important'), true); ?>
 			</div>
 		</div>
 	<?php endif; ?>
