ac25ce640d0d2bf097037564d9c6b7f0c172ce4a
Jan Žárský <jan.zarsky@gmail.com>
2014-11-30 15:28:27 +0100

    Create basic detail page

--- application/views/events/detail.php
+++ application/views/events/detail.php
@@ -1,1 +1,22 @@
-<?php $this->load->view('templates/event', array('event' => $event)); ?>
+<section class="event">
+	<div class="row">
+		<div class="col-sm-7">
+			<?php echo $this->load->view('templates/event_main_image.php', array('image_type' => 'large'), true); ?>
+		</div>
+		<div class="col-sm-5">
+			<div class="row">
+				<div class="col-sm-12">
+					<?php echo $this->load->view('templates/event_title.php', array('type' => null), true); ?>
+				</div>
+				<div class="col-sm-12">
+					<?php echo $this->load->view('templates/event_date.php', array('type' => null), true); ?>
+				</div>
+				<div class="col-sm-12">
+					<p class="event__text">
+						<?php echo $event['text']; ?>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
