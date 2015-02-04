bbb67e9eac75c8f5d19bf022be8edd094582e36f
Jan Žárský <jan.zarsky@gmail.com>
2015-02-04 10:19:44 +0100

    Create blank users page

---
+++ application/controllers/users.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Users extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('user_model');
+		$this->load->model('user_edit_model');
+	}
+
+	public function index()
+	{
+		$data['content'] = '';
+		
+		$header_data['page'] = 'users';
+		$header_data['name'] = $this->user_model->get_name();
+		$header_data['permissions'] = $this->user_model->get_permissions();
+		$data['header'] = $this->load->view('templates/admin_header', $header_data, true);
+		
+		$this->load->view('templates/admin', $data);
+	}
+}
---
+++ application/models/user_edit_model.php
@@ -0,0 +1,8 @@
+<?php
+class User_edit_model extends CI_Model {
+
+	public function __construct()
+	{
+		$this->load->database();
+	}
+}
