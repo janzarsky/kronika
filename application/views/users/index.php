30c33ef10e70cd7c3d318f6ea43031ab8ed08c89
Jan Žárský <jan.zarsky@gmail.com>
2015-02-07 13:51:31 +0100

    Link to add user

--- application/views/users/index.php
+++ application/views/users/index.php
@@ -1,51 +1,49 @@
-<div class="row">
-	<div class="col-sm-12">
-		<table class="table table-hover">
-			<thead>
-				<tr>
-					<th>Jméno</th>
-					<th>Email</th>
-					<th>Oprávnění</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach ($users as $user): ?>
-					<tr>
-						<td>
-							<a href="<?php echo base_url('/users/edit/' . $user['id']); ?>">
-								<?php echo $user['name']; ?>
-							</a>
-						</td>
+<a class="btn btn-primary" href="<?php echo base_url('/users/add'); ?>" role="button">Přidat uživatele</a>
+
+<table class="table table-hover">
+	<thead>
+		<tr>
+			<th>Jméno</th>
+			<th>Email</th>
+			<th>Oprávnění</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($users as $user): ?>
+			<tr>
+				<td>
+					<a href="<?php echo base_url('/users/edit/' . $user['id']); ?>">
+						<?php echo $user['name']; ?>
+					</a>
+				</td>
+				
+				<td>
+					<?php echo $user['email']; ?>
+				</td>
+				
+				<td>
+					<?php
+						$permissions = '';
 						
-						<td>
-							<?php echo $user['email']; ?>
-						</td>
+						if ($user['can_publish'])
+							$permissions .= 'publikovat, ';
 						
-						<td>
-							<?php
-								$permissions = '';
-								
-								if ($user['can_publish'])
-									$permissions .= 'publikovat, ';
-								
-								if ($user['can_approve'])
-									$permissions .= 'schvalovat, ';
-								
-								if ($user['can_edit_users'])
-									$permissions .= 'spravovat uživatele, ';
-								
-								if ($user['can_edit_settings'])
-									$permissions .= 'spravovat stránku, ';
-								
-								if (strlen($permissions) > 0)
-									$permissions = $permissions;
-								
-								echo $permissions;
-							?>
-						</td>
-					</tr>
-				<?php endforeach; ?>
-			</tbody>
-		</table>
-	</div>
-</div>
+						if ($user['can_approve'])
+							$permissions .= 'schvalovat, ';
+						
+						if ($user['can_edit_users'])
+							$permissions .= 'spravovat uživatele, ';
+						
+						if ($user['can_edit_settings'])
+							$permissions .= 'spravovat stránku, ';
+						
+						if (strlen($permissions) > 0)
+							$permissions = $permissions;
+						
+						echo $permissions;
+					?>
+				</td>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
