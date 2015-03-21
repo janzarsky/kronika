ea0eda9f38dcb626c9deb16f661265ab042deeb6
Jan Žárský <jan.zarsky@gmail.com>
2015-03-21 13:59:11 +0100

    Fix no image detail page

--- application/views/events/detail.php
+++ application/views/events/detail.php
@@ -21,52 +21,54 @@
 		</header>
 	</div>
 	
-	<div class="event__gallery <?php echo (count($event['media']) == 1) ? 'event__gallery--singleImage' : ''; ?>">
-		<div class="event__galleryRow">
-			<div class="event__navigation event__navigation--left">
-				<div class="event__navigationArrow">
-					<button><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span></button>
+	<?php if (count($event['media']) > 0 && $event['media'] != false): ?>
+		<div class="event__gallery <?php echo (count($event['media']) == 1) ? 'event__gallery--singleImage' : ''; ?>">
+			<div class="event__galleryRow">
+				<div class="event__navigation event__navigation--left">
+					<div class="event__navigationArrow">
+						<button><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span></button>
+					</div>
 				</div>
-			</div>
-			
-			<div class="event__galleryWrapper">
-				<?php $counter = 0; ?>
-				<?php foreach ($event['media'] as $media): ?>
-					<figure class="event__image <?php if ($counter == 0) echo 'event__image--active'; ?>">
-						<img srcset="<?php echo media_image($media['id'], 1600); ?> 1600w,
-												 <?php echo media_image($media['id'], 1200); ?> 1200w,
-												 <?php echo media_image($media['id'], 960); ?> 960w,
-												 <?php echo media_image($media['id'], 640); ?> 640w,
-												 <?php echo media_image($media['id'], 320); ?> 320w"
-							sizes="(min-width: 768px) 83vw,
-										 (min-width: 528px) 528px,
-										 100vw"
-							src="<?php echo media_image($media['id'], 960); ?>"
-							alt="" />
+				
+				<div class="event__galleryWrapper">
+					<?php $counter = 0; ?>
+					<?php foreach ($event['media'] as $media): ?>
+						<figure class="event__image <?php if ($counter == 0) echo 'event__image--active'; ?>">
+							<img srcset="<?php echo media_image($media['id'], 1600); ?> 1600w,
+													 <?php echo media_image($media['id'], 1200); ?> 1200w,
+													 <?php echo media_image($media['id'], 960); ?> 960w,
+													 <?php echo media_image($media['id'], 640); ?> 640w,
+													 <?php echo media_image($media['id'], 320); ?> 320w"
+								sizes="(min-width: 768px) 83vw,
+											 (min-width: 528px) 528px,
+											 100vw"
+								src="<?php echo media_image($media['id'], 960); ?>"
+								alt="" />
+							
+							<?php	if(isset($media['text']) && $media['text'] != ''): ?>
+								<figcaption class="event__imageText">
+									<?php echo $media['text']; ?>
+								</figcaption>
+							<?php endif; ?>
+						</figure>
 						
-						<?php	if(isset($media['text']) && $media['text'] != ''): ?>
-							<figcaption class="event__imageText">
-								<?php echo $media['text']; ?>
-							</figcaption>
+						<?php $counter++; ?>
+						
+						<?php if ($counter%2 == 0): ?>
+							<div class="clearfix visible-md-block"></div>
 						<?php endif; ?>
-					</figure>
-					
-					<?php $counter++; ?>
-					
-					<?php if ($counter%2 == 0): ?>
-						<div class="clearfix visible-md-block"></div>
-					<?php endif; ?>
-					<?php if ($counter%3 == 0): ?>
-						<div class="clearfix visible-lg-block"></div>
-					<?php endif; ?>
-				<?php endforeach; ?>
-			</div>
-			
-			<div class="event__navigation event__navigation--right">
-				<div class="event__navigationArrow">
-					<button><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
+						<?php if ($counter%3 == 0): ?>
+							<div class="clearfix visible-lg-block"></div>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				</div>
+				
+				<div class="event__navigation event__navigation--right">
+					<div class="event__navigationArrow">
+						<button><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
+	<?php endif; ?>
 </section>
